93f1a6008439ddb3aa19ecf8d288c8696f3aedd2
Rizki24-star <kikingar.567@gmail.com>
2024-08-22 21:16:52 +0700

    fix: fixing collaborators access

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -22,7 +22,7 @@ export const createDocument = async ({
     const room = await liveblocks.createRoom(roomId, {
       metadata,
       usersAccesses,
-      defaultAccesses: [],
+      defaultAccesses: ["room:write"],
     });
 
     return parseStringify(room);
