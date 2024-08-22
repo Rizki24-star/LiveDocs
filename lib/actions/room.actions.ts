cc9e7bd67ff4df6d23776848859c13d484a60500
Rizki24-star <kikingar.567@gmail.com>
2024-08-22 16:19:19 +0700

    feat: add collaborative editor room

---
+++ lib/actions/room.actions.ts
@@ -0,0 +1,55 @@
+"use server";
+import { nanoid } from "nanoid";
+import { liveblocks } from "../liveblocks";
+import { parseStringify } from "../utils";
+
+export const createDocument = async ({
+  userId,
+  email,
+}: CreateDocumentParams) => {
+  const roomId = nanoid();
+  try {
+    const metadata = {
+      creatorId: userId,
+      email,
+      title: "Untitled",
+    };
+
+    const usersAccesses: RoomAccesses = {
+      [email]: ["room:write"],
+    };
+
+    const room = await liveblocks.createRoom(roomId, {
+      metadata,
+      usersAccesses,
+      defaultAccesses: [],
+    });
+
+    return parseStringify(room);
+  } catch (error) {
+    console.log(`Error happened while creating document: ${error}`);
+  }
+};
+
+export const getDocument = async ({
+  roomId,
+  userId,
+}: {
+  roomId: string;
+  userId: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    // // TODO: Bring this back later on
+    // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+
+    // if (!hasAccess) {
+    //   throw new Error("You do not have access to this document");
+    // }
+
+    return parseStringify(room);
+  } catch (error) {
+    console.log(`Error happened while getting room : ${error}`);
+  }
+};
